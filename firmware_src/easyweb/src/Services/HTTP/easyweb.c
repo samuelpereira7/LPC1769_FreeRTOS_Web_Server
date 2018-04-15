237b7b3dcaaa99bf0c3605b01a3cd8c29aea6011
samuelpereira7 <samuelpereira@gec.inatel.br>
2018-04-15 14:57:33 -0300

    Includes e variáveis desnecessárias removidos do easyweb.c. Indentação aplicada.

--- firmware_src/easyweb/src/Services/HTTP/easyweb.c
+++ firmware_src/easyweb/src/Services/HTTP/easyweb.c
@@ -16,10 +16,8 @@
  ******************************************************************/
 
 // Modifications by Code Red Technologies for NXP LPC1769
-
 #include "stdlib.h"
 #include "stdio.h"
-#include "string.h"
 
 // CodeRed - added #define extern on next line (else variables
 // not defined). This has been done due to include the .h files 
@@ -28,9 +26,6 @@
 
 #include "easyweb.h"
 
-// CodeRed - removed header for original ethernet controller
-//#include "cs8900.c"                              // ethernet packet driver
-
 //CodeRed - added for LPC ethernet controller
 #include "ethmac.h"
 
@@ -38,18 +33,13 @@
 // #include "tcpip.c"                               // easyWEB TCP/IP stack
 #include "tcpip.h"                               // easyWEB TCP/IP stack
 
-
 // CodeRed - include renamed .h rather than .c file
 // #include "webside.c"                             // webside for our HTTP server (HTML)
 #include "webside.h"                             // webside for our HTTP server (HTML)
 
 #include "ew_systick.h"
 #include "Temperature.h"
-#include "Button.h"
 #include "Accelerometer.h"
-#include "Trimpot.h"
-#include "RGB_Leds.h"
-#include "OLED_display.h"
 
 // This function implements a very simple dynamic HTTP-server.
 // It waits until connected, then sends a HTTP-header and the
@@ -59,107 +49,106 @@
 // not work. In this case, simply add some extra lines
 // (e.g. CR and LFs) to the HTML-code.
 
-const unsigned char GetResponse[] =              // 1st thing our server sends to a client
+const unsigned char GetResponse[] =   // 1st thing our server sends to a client
+		{ "HTTP/1.0 200 OK\r\n"     // protocol ver 1.0, code 200, reason OK
+						"Content-Type: text/html\r\n"// type of data we want to send
+						"\r\n"               // indicate end of HTTP-header
+		};
+
+void HTTPServer( void )
 {
-  "HTTP/1.0 200 OK\r\n"                          // protocol ver 1.0, code 200, reason OK
-  "Content-Type: text/html\r\n"                  // type of data we want to send
-  "\r\n"                                         // indicate end of HTTP-header
-};
+	if (SocketStatus & SOCK_CONNECTED) // check if somebody has connected to our TCP
+	{
+		if (SocketStatus & SOCK_DATA_AVAILABLE) // check if remote TCP sent data
+			TCPReleaseRxBuffer();                      // and throw it away
 
-void HTTPServer(void)
-{
-  if (SocketStatus & SOCK_CONNECTED)             // check if somebody has connected to our TCP
-  {
-    if (SocketStatus & SOCK_DATA_AVAILABLE)      // check if remote TCP sent data
-      TCPReleaseRxBuffer();                      // and throw it away
+		if (SocketStatus & SOCK_TX_BUF_RELEASED) // check if buffer is free for TX
+		{
+			if (!(HTTPStatus & HTTP_SEND_PAGE)) // init byte-counter and pointer to webside
+			{                              // if called the 1st time
+				HTTPBytesToSend = sizeof(WebSide) - 1; // get HTML length, ignore trailing zero
+				PWebSide = (unsigned char *) WebSide; // pointer to HTML-code
+			}
 
-    if (SocketStatus & SOCK_TX_BUF_RELEASED)     // check if buffer is free for TX
-    {
-      if (!(HTTPStatus & HTTP_SEND_PAGE))        // init byte-counter and pointer to webside
-      {                                          // if called the 1st time
-        HTTPBytesToSend = sizeof(WebSide) - 1;   // get HTML length, ignore trailing zero
-        PWebSide = (unsigned char *)WebSide;     // pointer to HTML-code
-      }
+			if (HTTPBytesToSend > MAX_TCP_TX_DATA_SIZE) // transmit a segment of MAX_SIZE
+			{
+				if (!(HTTPStatus & HTTP_SEND_PAGE)) // 1st time, include HTTP-header
+				{
+					memcpy( TCP_TX_BUF, GetResponse, sizeof(GetResponse) - 1 );
+					memcpy( TCP_TX_BUF + sizeof(GetResponse) - 1, PWebSide,
+					MAX_TCP_TX_DATA_SIZE - sizeof(GetResponse) + 1 );
+					HTTPBytesToSend -= MAX_TCP_TX_DATA_SIZE
+							- sizeof(GetResponse) + 1;
+					PWebSide += MAX_TCP_TX_DATA_SIZE - sizeof(GetResponse) + 1;
+				}
+				else
+				{
+					memcpy( TCP_TX_BUF, PWebSide,
+					MAX_TCP_TX_DATA_SIZE );
+					HTTPBytesToSend -= MAX_TCP_TX_DATA_SIZE;
+					PWebSide += MAX_TCP_TX_DATA_SIZE;
+				}
 
-      if (HTTPBytesToSend > MAX_TCP_TX_DATA_SIZE)     // transmit a segment of MAX_SIZE
-      {
-        if (!(HTTPStatus & HTTP_SEND_PAGE))           // 1st time, include HTTP-header
-        {
-          memcpy(TCP_TX_BUF, GetResponse, sizeof(GetResponse) - 1);
-          memcpy(TCP_TX_BUF + sizeof(GetResponse) - 1, PWebSide, MAX_TCP_TX_DATA_SIZE - sizeof(GetResponse) + 1);
-          HTTPBytesToSend -= MAX_TCP_TX_DATA_SIZE - sizeof(GetResponse) + 1;
-          PWebSide += MAX_TCP_TX_DATA_SIZE - sizeof(GetResponse) + 1;
-        }
-        else
-        {
-          memcpy(TCP_TX_BUF, PWebSide, MAX_TCP_TX_DATA_SIZE);
-          HTTPBytesToSend -= MAX_TCP_TX_DATA_SIZE;
-          PWebSide += MAX_TCP_TX_DATA_SIZE;
-        }
-          
-        TCPTxDataCount = MAX_TCP_TX_DATA_SIZE;   // bytes to xfer
-        InsertDynamicValues();                   // exchange some strings...
-        TCPTransmitTxBuffer();                   // xfer buffer
-      }
-      else if (HTTPBytesToSend)                  // transmit leftover bytes
-      {
-        memcpy(TCP_TX_BUF, PWebSide, HTTPBytesToSend);
-        TCPTxDataCount = HTTPBytesToSend;        // bytes to xfer
-        InsertDynamicValues();                   // exchange some strings...
-        TCPTransmitTxBuffer();                   // send last segment
-        TCPClose();                              // and close connection
-        HTTPBytesToSend = 0;                     // all data sent
-      }
+				TCPTxDataCount = MAX_TCP_TX_DATA_SIZE; // bytes to xfer
+				InsertDynamicValues();  // exchange some strings...
+				TCPTransmitTxBuffer();               // xfer buffer
+			}
+			else if (HTTPBytesToSend)         // transmit leftover bytes
+			{
+				memcpy( TCP_TX_BUF, PWebSide, HTTPBytesToSend );
+				TCPTxDataCount = HTTPBytesToSend;   // bytes to xfer
+				InsertDynamicValues();  // exchange some strings...
+				TCPTransmitTxBuffer();         // send last segment
+				TCPClose();                 // and close connection
+				HTTPBytesToSend = 0;                // all data sent
+			}
 
-      HTTPStatus |= HTTP_SEND_PAGE;              // ok, 1st loop executed
-    }
-  }
-  else
-    HTTPStatus &= ~HTTP_SEND_PAGE;               // reset help-flag if not connected
+			HTTPStatus |= HTTP_SEND_PAGE;       // ok, 1st loop executed
+		}
+	}
+	else
+		HTTPStatus &= ~HTTP_SEND_PAGE;       // reset help-flag if not connected
 }
 
 // Code Red - new version of InsertDynamicValues()
-void InsertDynamicValues(void)
+void InsertDynamicValues( void )
 {
 	unsigned char *Key;
 	char NewKey[6];
-    unsigned int i;
-  
-  if (TCPTxDataCount < 4) return;                     // there can't be any special string
-  
-  Key = TCP_TX_BUF;
+	unsigned int i;
 
-  for (i = 0; i < (TCPTxDataCount - 3); i++)
-  {
-    if (*Key == 'A')
-     if (*(Key + 1) == 'C')
-		 switch (*(Key + 2))
-		 {
-		   case 'X' :
-		   {
-			 sprintf(NewKey, "%04d", Accelerometer_getX());
-			 memcpy(Key, NewKey, 4);
-			 break;
-		   }
-		   case 'Y' :
-		   {
-			 sprintf(NewKey, "%04d", Accelerometer_getY());
-			 memcpy(Key, NewKey, 4);
-			 break;
-		   }
-		   case 'Z' :
-		   {
-			 sprintf(NewKey, "%04d", Accelerometer_getZ());
-			 memcpy(Key, NewKey, 4);
-			 break;
-		   }
-		   case 'T' :
-		   {
-			 sprintf(NewKey, "%03u", Temperature_read());
-			 memcpy(Key, NewKey, 3);
-			 break;
-		   }
-		 }
-    Key++;
-  }
+	if (TCPTxDataCount < 4)
+		return;                     // there can't be any special string
+
+	Key = TCP_TX_BUF;
+
+	for (i = 0; i < (TCPTxDataCount - 3); i++)
+	{
+		if (*Key == 'A')
+		{
+			if (*(Key + 1) == 'C')
+			{
+				switch (*(Key + 2))
+				{
+					case 'X':
+						sprintf( NewKey, "%04d", Accelerometer_getX() );
+						memcpy( Key, NewKey, 4 );
+						break;
+					case 'Y':
+						sprintf( NewKey, "%04d", Accelerometer_getY() );
+						memcpy( Key, NewKey, 4 );
+						break;
+					case 'Z':
+						sprintf( NewKey, "%04d", Accelerometer_getZ() );
+						memcpy( Key, NewKey, 4 );
+						break;
+					case 'T':
+						sprintf( NewKey, "%03d", Temperature_read() );
+						memcpy( Key, NewKey, 3 );
+						break;
+				}
+			}
+			Key++;
+		}
+	}
 }
